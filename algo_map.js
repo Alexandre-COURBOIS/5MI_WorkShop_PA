c58c14d81deb4292eb37460bc3dd1bd7319ecdc8
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2023-09-04 09:25:39 +0200

    Initial commit

---
+++ algo_map.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoid2ViYXBzeSIsImEiOiJjazhlYXk1ejkxNGFpM2dsdjJkaDd2b2RmIn0.pWabX6z0Us-G8OiF9DhuNA';
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [1.08333, 49.433331],
+    zoom: 10
+});
